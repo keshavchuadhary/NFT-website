aaed0ee042c78a1f5ccaa1231f1bacc8aea6ed50
Keshav <keshavchy9841@gmail.com>
2024-08-29 21:43:40 +0545

    navbar component added

--- components/componentindex.js
+++ components/componentindex.js
@@ -1,10 +1,9 @@
 "use-client";
 import Navbar from "./Navbar/Navbar";
-import Discover from "./Navbar/Discover/Discover";
-import HelpCenter from "./Navbar/HelpCenter/HelpCenter";
-import Notification from "./Navbar/Notification/Notification";
-import Button from "./Button/Button";
-import Profile from "./Navbar/Profile/Profile";
-import Sidebar from "./Navbar/Sidebar/Sidebar";
 
-export  { Navbar, Discover, HelpCenter, Notification, Button, Profile, Sidebar };
+
+
+import Button from "./Button/Button";
+import Footer from "./Footer/Footer";
+
+export  { Navbar, Button, Footer };
